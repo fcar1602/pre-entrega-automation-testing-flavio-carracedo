bf96f900cbd9cdc19e4af2a7a42505062c472da3
fcar1602 <flacarracedo@gmail.com>
2025-12-02 12:12:19 -0300

    Disable Chrome Option

--- conftest.py
+++ conftest.py
@@ -6,16 +6,37 @@ import logging
 import io
 from selenium import webdriver
 
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    options = Options()
+
+    prefs = {
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False,
+
+        "autofill.profile_enabled": False,
+        "autofill.password_manager_enabled": False,
+        "autofill.credit_card_enabled": False,
+        "autofill.address_enabled": False,
+
+        "profile.password_manager_leak_detection": False,
+    }
+
+    options.add_experimental_option("prefs", prefs)
+
+    options.add_argument(
+        "--disable-features=PasswordManagerEnabled,PasswordManagerUI"
+    )
+
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     yield driver
     driver.quit()
 
-def pytest_configure(config):
-    global pytest_html
-    pytest_html = config.pluginmanager.getplugin("html")
 
 # optional logger fixture (kept as fallback)
 
